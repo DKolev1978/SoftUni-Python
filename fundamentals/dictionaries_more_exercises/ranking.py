1a114eef08aa3a7cbf6ba7d3644a8572471cfa35
Dimitar Kolev <dimitar.r.kolev@gmail.com>
2023-03-15 15:34:20 +0200

    Test

--- fundamentals/dictionaries_more_exercises/ranking.py
+++ fundamentals/dictionaries_more_exercises/ranking.py
@@ -1,8 +1,9 @@
 contest_password = {}
 contest_users = {}
 user_input = input()
-
-
+best_students = {}
+total_points = 0
+sum_points = 0
 while True:
     if user_input == "end of contests":
         break
@@ -19,21 +20,38 @@ while True:
     points = int(points)
     if contest in contest_password:
         if password == contest_password[contest]:
-            contest_users[contest] = {username: points}
-            if contest_users[contest][username] < points:
-                contest_users[contest][username] = points
-            # if contest_users[contest][username] > points:
-            #     pass
-            # else:
-            #     contest_users[contest][username] = points
-            print(contest_users[contest][username])
-    #  "{contest}=>{password}=>{username}=>{points}"
+            username_score = {username: points}
+            if contest not in contest_users:
+                contest_users[contest] = username_score
+            else:
+                contest_users[contest].update(username_score)
+                for key, value in contest_users.items():
+                    for k, v in value.items():
+                        if v < points:
+                            v = points
 
     user_input = input()
-print(contest_password)
+
+for key, value in contest_users.items():
+    if value and username in value.keys():
+        sum_points += value[username]
+        best_students =
+print(sum_points)
+
+
 print(contest_users)
 
-
-
-
-
+# # Python code to find sum values within nested dictionaries
+# weapons = {'': None, 'sword': {'steel': 151,
+#                                'sharpness': 100,
+#                                'age': 2, },
+#
+#            'arrow': {'steel': 120,
+#                      'sharpness': 205,
+#                      'age': 1, }}
+#
+# sumValue1 = sum(d['sharpness'] for d in weapons.values() if d)
+# sumValue2 = sum(d['steel'] for d in weapons.values() if d)
+#
+# print(sumValue1)
+# print(sumValue2)
