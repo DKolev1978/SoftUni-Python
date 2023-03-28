b8ee110c12f42135a2b451d4825c2abbf779c46a
Dimitar Kolev <dimitar.r.kolev@gmail.com>
2023-03-28 15:50:22 +0300

    New Task

--- fundamentals/regular_expressions_more_exercises/star_enigma_home.py
+++ fundamentals/regular_expressions_more_exercises/star_enigma_home.py
@@ -1,8 +1,12 @@
-n = int(input())
+import re
+planet = {}
 magic_letters = ["s","t","a","r"]
-letter_count = 0
-
+n = int(input())
+planet_name = r"@([A-Za-z]+)"
+population_count = r":(\d+)"
+attack_type = r""
 for i in range(n):
+    letter_count = 0
     text = input()
     new_message = ""
     for letter in text:
@@ -11,7 +15,9 @@ for i in range(n):
     for letter in text:
         new_letter = ord(letter) - letter_count
         new_message += chr(new_letter)
-
-
+    name = re.findall(planet_name, new_message)
+    population = re.findall(population_count, new_message)
+    print(*name)
+    print(*population)
     print(new_message)
     print(letter_count)
