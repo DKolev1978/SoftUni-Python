49f957b4eec5f52c96f1128fdb241dae04e75441
Dimitar Kolev <110812306+DKolev1978@users.noreply.github.com>
2023-03-29 18:07:24 +0300

    New tasks

--- fundamentals/regular_expressions_more_exercises/star_enigma_home.py
+++ fundamentals/regular_expressions_more_exercises/star_enigma_home.py
@@ -1,10 +1,12 @@
 import re
-planet = {}
+attacked_planets = []
+destroyed_plannet = []
 magic_letters = ["s","t","a","r"]
 n = int(input())
-planet_name = r"@([A-Za-z]+)"
-population_count = r":(\d+)"
-attack_type = r""
+planet_name = r".*@(?P<planet>[A-Z][a-z]+)[^\@\-\!\:\>]*:)"
+population_count = r"(\d+)[^\@\-\!\:\>]*\!])"
+attack_type = r"(A|D)\![^\@\-\!\:\>]*\->]"
+soldier_count = r"(\d+).*"
 for i in range(n):
     letter_count = 0
     text = input()
@@ -17,7 +19,21 @@ for i in range(n):
         new_message += chr(new_letter)
     name = re.findall(planet_name, new_message)
     population = re.findall(population_count, new_message)
-    print(*name)
-    print(*population)
-    print(new_message)
-    print(letter_count)
+    attack = re.findall(attack_type, new_message)
+    soldier = re.findall(soldier_count, new_message)
+    if not len(name) or not len(population) or not len(attack) or not len(soldier):
+        continue
+    else:
+        if attack[0] == "A":
+            attacked_planets.append(name[0])
+        elif attack[0] == "D":
+            destroyed_plannet.append(name[0])
+
+print(f"Attacked planets: {len(attacked_planets)}")
+for planet in sorted(attacked_planets):
+    print(f"-> {planet}")
+print(f"Destroyed planets: {len(destroyed_plannet)}")
+for planet in sorted(destroyed_plannet):
+    print(f"-> {planet}")
+
+
