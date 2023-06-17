75c72bf0cd3557b2c5eec1ac44a139c6959940b3
Dimitar Kolev <110812306+DKolev1978@users.noreply.github.com>
2023-06-17 12:47:09 +0300

    Update play

--- python_advanced/play.py
+++ python_advanced/play.py
@@ -1,2 +1,30 @@
 text = int(input())
 
+directions = {
+    "up": lambda r, c: [(r - 1) % SIZE, c],
+    "down": lambda r, c: [(r + 1) % SIZE, c],
+    "left": lambda r, c: [r, (c - 1) % SIZE],
+    "right": lambda r, c: [r, (c + 1) % SIZE],
+}
+
+
+directions_simple = {
+    'up': (-1, 0),
+    'down': (1, 0),
+    'left': (0, -1),
+    'right': (0, 1),
+}
+n = int(input())
+
+easter_matrix = []
+bunny_position = []
+best_path = []
+
+best_direction = None
+max_coll_eggs = 0
+
+for r in range(n):
+    easter_matrix.append(input().split())
+
+    if 'B' in easter_matrix[r]:
+        bunny_pos = [r, easter_matrix[r].index('B')]
