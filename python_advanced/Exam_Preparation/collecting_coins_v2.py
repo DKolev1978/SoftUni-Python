b671409f7d39bd10eeb3b5ab196fa7be94c7f93a
Dimitar Kolev <110812306+DKolev1978@users.noreply.github.com>
2023-08-07 21:45:22 +0300

    New

--- python_advanced/Exam_Preparation/collecting_coins_v2.py
+++ python_advanced/Exam_Preparation/collecting_coins_v2.py
@@ -1,60 +1,60 @@
-import math
-
-
 def calculate_position(ma, row, col):
     if row < 0:
         row = len(ma) - 1
     if col < 0:
         col = len(ma) - 1
-
     if row >= len(ma):
         row = 0
     if col >= len(ma):
         col = 0
-
     return row, col
 
 
 n = int(input())
-coins = 0
+
 matrix = []
 player_position = []
-player_path = []
-is_winner = True
-for row_odx in range(n):
+
+for row_index in range(n):
     row_data = input().split()
     if "P" in row_data:
-        player_position = [row_odx, row_data.index("P")]
+        player_position = [row_index, row_data.index("P")]
     matrix.append(row_data)
-
+is_winner = True
+player_path = []
 player_path.append(player_position)
+coins = 0
+
 while coins < 100:
+
     command = input()
     current_row, current_col = player_position
-    if command == 'up':
+    if command == "up":
         row, col = calculate_position(matrix, current_row - 1, current_col)
-    if command == 'down':
+    elif command == "down":
         row, col = calculate_position(matrix, current_row + 1, current_col)
-    if command == "right":
+    elif command == "right":
         row, col = calculate_position(matrix, current_row, current_col + 1)
-    if command == 'left':
+    elif command == "left":
         row, col = calculate_position(matrix, current_row, current_col - 1)
 
-    matrix[current_row][current_col] = '0'
+    matrix[current_row][current_col] = "0"
     if matrix[row][col] == "X":
-        coins //= 2
+        player_path.append([row, col])
         is_winner = False
-        player_path.append(player_position)
-        print(f"Game over! You've collected {coins} coins.")
+        coins //= 2
         break
 
     coins += int(matrix[row][col])
-
-    matrix[row][col] = 'P'
+    matrix[row][col] = "P"
     player_position = [row, col]
     player_path.append(player_position)
-
 if is_winner:
     print(f"You won! You've collected {coins} coins.")
+else:
+    print(f"Game over! You've collected {coins} coins.")
+
 print("Your path:")
-[print(step) for step in player_path]
+for el in player_path:
+    print(el)
+# [print(step) for step in player_path]
