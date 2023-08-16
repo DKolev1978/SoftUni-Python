3c525a164561b95f72a2b1d68e95c22643d51cb4
Dimitar Kolev <110812306+DKolev1978@users.noreply.github.com>
2023-08-16 21:19:18 +0300

    Test

--- OOP/retake_exam/01. Structure_Skeleton/project/teams/outdoor_team.py
+++ OOP/retake_exam/01. Structure_Skeleton/project/teams/outdoor_team.py
@@ -5,6 +5,7 @@ class OutdoorTeam(BaseTeam):
 
     def __init__(self, name: str, country: str, advantage: int, budget: float = 1_000.0):
         super().__init__(name, country, advantage, budget)
+        # self.equipment: list = []
 
     def win(self):
         self.advantage += 115
--- OOP/retake_exam/01. Structure_Skeleton/project/tournament.py
+++ OOP/retake_exam/01. Structure_Skeleton/project/tournament.py
@@ -27,14 +27,11 @@ class Tournament:
 
         self.__name = value
 
-    def create_team(self, team_type: str, team_name: str, country: str, advantage: int):
-        return self.VALID_TEAM_TYPES[team_type](team_name, country, advantage)
-
     def add_equipment(self, equipment_type: str):
         if equipment_type not in BaseEquipment.VALID_EQUIPMENT_TYPES:
             raise Exception("Invalid equipment type!")
-
-        self.equipment.append(equipment_type)
+        eq_type = self.VALID_EQUIPMENT[equipment_type](equipment_type)
+        self.equipment.append(eq_type)
         return f"{equipment_type} was successfully added."
 
     def add_team(self, team_type: str, team_name: str, country: str, advantage: int):
@@ -42,47 +39,61 @@ class Tournament:
             raise Exception("Invalid team type!")
         if self.capacity == 0:
             raise Exception("Not enough tournament capacity.")
-        new_team = self.create_team(team_type, team_name, country, advantage)
+        new_team = self.VALID_TEAM_TYPES[team_type](team_name, country, advantage)
         self.teams.append(new_team)
         return f"{team_type} was successfully added."
 
-    def get_team_type(self, team_name):
-        team_type = [name for name in self.teams if name.name == team_name]
-        return team_type[0]
+    def _get_team(self, team_name):
+        cur_team = [team for team in self.teams if team.name == team_name]
+        if cur_team:
+            return cur_team[0]
+        return None
 
-    def get_team_by_name(self, team_name):
-        team_name = [name for name in self.teams if name.name == team_name]
-        return team_name[0]
+    def _calculate_equipment_prices(self, equipment_type):
+        equipment_type_price = 0
+        for eq_type in self.equipment:
+            if eq_type == "KneePad":
+                equipment_type_price += 15
+            else:
+                equipment_type_price += 25
+        return equipment_type_price
+
+    def _get_equipment(self, equipment_type):
+        cur_eq = [eq for eq in self.equipment if eq.__class__.__name__ == equipment_type]
+        return cur_eq[0] if cur_eq else None
 
     def sell_equipment(self, equipment_type: str, team_name: str):
-        current_team_type = self.get_team_type(team_name)
-        if self.VALID_EQUIPMENT_TYPES_PRICES[equipment_type] > current_team_type.budget:
+        current_team = self._get_team(team_name)
+        cur_eq = self._get_equipment(equipment_type)
+        equipment_type_price = self._calculate_equipment_prices(equipment_type)
+        if equipment_type_price > current_team.budget:
             raise Exception("Budget is not enough!")
 
-        equipment = self.equipment.remove(equipment_type)
-        team_name_current = self.get_team_by_name(team_name)
-        team_name_current.equipment.append(equipment)
-        team_name_current.budget -= self.VALID_EQUIPMENT_TYPES_PRICES[equipment_type]
+        current_team.budget -= equipment_type_price
+        self.equipment.remove(cur_eq)
+        current_team.equipment.append(cur_eq)
+        print(equipment_type_price, "Fuck you")
         return f"Successfully sold {equipment_type} to {team_name}."
 
     def remove_team(self, team_name: str):
-        if not self.get_team_by_name(team_name):
+        team_for_removal = self._get_team(team_name)
+        if not team_for_removal:
             raise Exception("No such team!")
-        wins = self.get_team_by_name(team_name)
-        if wins.wins > 0:
-            raise Exception(f"The team has {wins.wins} wins! Removal is impossible!")
-        current_team_name = self.get_team_by_name(team_name)
-        self.teams.remove(current_team_name)
-        return f"Successfully removed {team_name}."
+        if team_for_removal.wins > 0:
+            raise Exception(f"The team has {team_for_removal.wins} wins! Removal is impossible!")
+        self.teams.remove(team_for_removal)
+        return f"Successfully removed {team_for_removal.name}."
 
     def increase_equipment_price(self, equipment_type: str):
-        equipment_to_raise_prices = self.VALID_EQUIPMENT[equipment_type]
-        equipment_to_raise_prices.increase_price()
-        for el in self.equipment:
-            equipment_count = 0
-            if equipment_type == el:
-                equipment_count += 1
-        return f"Successfully changed {equipment_count}pcs of equipment."
+        pass
+        # equipment_to_raise_prices = self._get_equipment(equipment_type)
+        # print(equipment_to_raise_prices.price)
+        # equipment_to_raise_prices.increase_price()
+        # for el in self.equipment:
+        #     equipment_count = 0
+        #     if equipment_type == el:
+        #         equipment_count += 1
+        # return f"Successfully changed {equipment_count}pcs of equipment."
 
     def play(self, team_name1: str, team_name2: str):
         pass
@@ -93,14 +104,13 @@ class Tournament:
                f"Teams:"
 
 
-
-
 t = Tournament('SoftUniada2023', 2)
 
 print(t.add_equipment('KneePad'))
 print(t.add_equipment('ElbowPad'))
 
 print(t.add_team('OutdoorTeam', 'Levski', 'BG', 250))
+
 print(t.add_team('OutdoorTeam', 'Spartak', 'BG', 250))
 print(t.add_team('IndoorTeam', 'Dobrich', 'BG', 280))
 
